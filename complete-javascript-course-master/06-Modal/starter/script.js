2ef08013ff46cbc2738750689fed12f7ded09d4c
Sabra <100771060+Sabraaa@users.noreply.github.com>
2024-07-21 12:44:02 +0330

    combine two if statements

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -8,7 +8,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-}
+};
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -20,11 +20,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-document.addEventListener('keydown', function(e){
-// console.log(e.key)
-if (e.key==='Escape') {
-if (!modal.classList.contains('hidden')) {
-  closeModal()
-}
-}
-})
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key)
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
