87eec4c70bce5944a75232f6a67e4183bca2cb6c
Sabra <100771060+Sabraaa@users.noreply.github.com>
2024-07-21 12:40:54 +0330

    close model with Escape key

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
@@ -20,5 +19,12 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function(e){
+// console.log(e.key)
+if (e.key==='Escape') {
+if (!modal.classList.contains('hidden')) {
+  closeModal()
+}
+}
+})
