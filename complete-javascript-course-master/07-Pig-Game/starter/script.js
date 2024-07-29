de9b203aac98489175fab0d320f051a8ac073457
Sabra <100771060+Sabraaa@users.noreply.github.com>
2024-07-29 14:52:17 +0330

    dice roll

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,4 +1,27 @@
 'use strict';
 
-const score0 = document.querySelector('#score--0')
-const 
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  //1.generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //2. display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //3. 1: switch to next player
+  if (dice !== 1) {
+
+  } else {
+    
+  }
+});
