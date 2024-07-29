02f40e336ce26082fe86953a60b8be56719df9ec
Sabra <100771060+Sabraaa@users.noreply.github.com>
2024-07-29 16:57:31 +0330

    created the conditional state of the game  both if the dice is 1 or it is anything else

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -13,7 +14,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   //1.generate random dice roll
@@ -26,9 +29,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to current
     currentScore += dice;
-    current0El.textContent= currentScore;
-
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
   }
 });
